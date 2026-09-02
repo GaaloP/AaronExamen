bdb51ea1a1754aedf8dc2dd11db4805ac649c2ca
Carlos161104 <canoja161104@gmail.com>
2026-09-01 18:21:41 -0600

    feat: create tickets entity

---
+++ apps/back/src/app/tickets/ticket.entity.ts
@@ -0,0 +1,43 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../users/user.entity';
+
+export enum TicketStatus {
+    OPEN = 'open',
+    IN_PROGRESS = 'in_progress',
+    CLOSED = 'closed',
+}
+
+@Entity()
+export class Ticket {
+    @PrimaryGeneratedColumn('uuid')
+    uuid?: string;
+
+    @Column({ type: 'int', generated: 'increment', unique: true })
+    ticketCode?: number;
+
+    @Column()
+    category?: string;
+
+    @Column({ type: 'enum', enum: TicketStatus, default: TicketStatus.OPEN })
+    status?: TicketStatus;
+
+    @ManyToOne(() => User, { eager: false, nullable: false })
+    @JoinColumn({ name: 'assignedToUuid' })
+    assignedTo?: User;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    openAt?: Date;
+
+    @Column({ type: 'timestamp', nullable: true })
+    closedAt?: Date | null;
+
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    comments?: string | null;
+}
