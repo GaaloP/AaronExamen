3f26dd731573fb583809c591379f5ec9ea6ce66b
GaaloP <gposada24@alumnos.uaq.mx>
2026-09-02 22:18:00 -0600

    feat: add reoles guard

--- apps/back/src/app/auth/strategies/jwt.strategy.ts
+++ apps/back/src/app/auth/strategies/jwt.strategy.ts
@@ -12,12 +12,11 @@ interface JwtPayload {
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     
-    // constructor(private readonly configService: ConfigService) {
-    constructor(configService: ConfigService) {
+    constructor(private readonly configService: ConfigService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: configService.get('JWT_SECRET') || 'default_secret',
+            secretOrKey: configService.get('JWT_SECRET')!,
         });
     }
 
